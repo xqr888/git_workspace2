890ec2d6123824d3b07e9b081e74ea46196134bc
xqrui <1125162688@qq.com>
2022-01-22 13:03:13 +0800

    提交文件

---
+++ HelloGit/src/main/java/com/atguigu/git/HelloGit.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author xqrui
+ * @date 2022/1/21
+ * @dec 描述
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
