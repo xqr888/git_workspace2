5e5180daa7b15df21ddbe66e11a28c3085d87841
xqrui <1125162688@qq.com>
2024-01-21 15:09:23 +0800

    push commit

--- HelloGit/src/main/java/com/atguigu/git/HelloGit.java
+++ HelloGit/src/main/java/com/atguigu/git/HelloGit.java
@@ -9,6 +9,9 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("更新1!");
+        System.out.println("更新2!");
+        System.out.println("更新3!");
+        System.out.println("pushtest!");
 
     }
 }
